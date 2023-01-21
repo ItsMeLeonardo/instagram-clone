34c4f88d430104ee1a2a5787abb762a1f39b0d9a
itsmeleonardo <carlosgamercovi20@gmail.com>
2023-01-21 18:32:54 -0500

    complete post

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,5 +1,102 @@
-import React from 'react'
+import Play from 'remixicon-react/PlayFillIcon'
+import AddIcon from 'remixicon-react/AddFillIcon'
+
+import Avatar from 'components/shared/Avatar'
+
+import styles from './page-style.module.css'
+import Post from 'components/shared/Post'
+
+type Story = {
+  id: string
+  name: string
+  avatar: string
+}
+
+const DATA = [
+  {
+    id: '1',
+    name: 'Perdana',
+    avatar: 'https://i.pinimg.com/236x/c8/0f/97/c80f971dca149720d06b5e134e6b4a7d.jpg',
+  },
+  {
+    id: '2',
+    name: 'Anna',
+    avatar: 'https://i.pinimg.com/236x/31/81/d9/3181d9e80494f5d14e65f9aecb07c792.jpg',
+  },
+  {
+    id: '3',
+    name: 'Vera',
+    avatar: 'https://i.pinimg.com/236x/3a/72/58/3a72584976987b1458c7330eb5638966.jpg',
+  },
+  {
+    id: '4',
+    name: 'Vera',
+    avatar: 'https://i.pinimg.com/236x/7f/05/71/7f0571fb0e693ced5da9b80fbd9adcf7.jpg',
+  },
+  {
+    id: '5',
+    name: 'Vera',
+    avatar: 'https://i.pinimg.com/236x/22/29/8b/22298b7a73407463bb3b7283f0c7714d.jpg',
+  },
+  {
+    id: '6',
+    name: 'Anna',
+    avatar: 'https://i.pinimg.com/236x/31/81/d9/3181d9e80494f5d14e65f9aecb07c792.jpg',
+  },
+]
 
 export default function page() {
-  return <div>page</div>
+  return (
+    <div className={styles.container}>
+      <section className={styles.section}>
+        <header className={styles.header}>
+          <h3 className={styles.title}>Stories</h3>
+
+          <button className={styles.see_all}>
+            <span className={styles.label}>Watch all</span>
+            <span className={styles.icon}>
+              <Play size="20" />
+            </span>
+          </button>
+        </header>
+
+        <aside className={styles.stories}>
+          <button className={styles.story}>
+            <Avatar bordered size="lg" icon={<AddIcon size="28" />} alt="afkajf" />
+
+            <span className={styles.label}>Add story</span>
+          </button>
+          {DATA.map((story: Story) => (
+            <button key={story.id} className={styles.story}>
+              <Avatar size="lg" bordered src={story.avatar} alt={story.name} />
+
+              <span className={styles.label}>{story.name}</span>
+            </button>
+          ))}
+        </aside>
+      </section>
+
+      <section className={`${styles.section} ${styles.feed}`}>
+        <header className={styles.header}>
+          <h3 className={styles.title}>Feeds</h3>
+
+          <div className={styles.filters}>
+            <button className={styles.button}>Latest</button>
+            <button className={styles.button} data-active={true}>
+              Popular
+            </button>
+          </div>
+        </header>
+
+        <aside className={styles.posts}>
+          <Post postImage="https://images.unsplash.com/photo-1545569341-9eb8b30979d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8amFwYW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" />
+
+          <Post postImage="https://images.unsplash.com/photo-1480796927426-f609979314bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8amFwYW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" />
+          <Post postImage="https://images.unsplash.com/photo-1492571350019-22de08371fd3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8amFwYW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" />
+          <Post postImage="https://images.unsplash.com/photo-1526481280693-3bfa7568e0f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8amFwYW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" />
+          <Post postImage="https://images.unsplash.com/photo-1542051841857-5f90071e7989?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8amFwYW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" />
+        </aside>
+      </section>
+    </div>
+  )
 }
---
+++ components/shared/Post/index.tsx
@@ -0,0 +1,100 @@
+import Options from 'remixicon-react/MoreFillIcon'
+import Heart from 'remixicon-react/Heart3LineIcon'
+import Comment from 'remixicon-react/Chat1LineIcon'
+import Share from 'remixicon-react/ShareLineIcon'
+import Bookmark from 'remixicon-react/BookMarkLineIcon'
+import Attachment from 'remixicon-react/Attachment2Icon'
+import Emotion from 'remixicon-react/EmotionHappyLineIcon'
+import ImageIcon from 'remixicon-react/ImageLineIcon'
+
+import User from 'components/shared/User'
+import Avatar from 'components/shared/Avatar'
+
+export type PostProps = {
+  user?: string
+  postImage: string
+}
+
+import styles from './Post.module.css'
+
+export default function Post(props: PostProps) {
+  const { postImage } = props
+  return (
+    <section className={styles.post}>
+      <header className={styles.header}>
+        <a href="/">
+          <User
+            avatar="https://i.pinimg.com/236x/ab/38/69/ab38691fb2e67fa2553f77042a128f3c.jpg"
+            name="Anghelina"
+            location="Ukraine"
+            interactive
+          />
+        </a>
+
+        <button className={styles.options}>
+          <Options />
+        </button>
+      </header>
+      <div className={styles.body}>
+        <picture className={styles.image}>
+          <img src={postImage} alt="post" />
+        </picture>
+
+        <div className={styles.options}>
+          <button className={styles.button}>
+            <span className={styles.icon}>
+              <Heart size="20" />
+            </span>
+            <span className={styles.label}>28.5k Like</span>
+          </button>
+          <button className={styles.button}>
+            <span className={styles.icon}>
+              <Comment size="20" />
+            </span>
+            <span className={styles.label}>33 Comment</span>
+          </button>
+          <button className={styles.button}>
+            <span className={styles.icon}>
+              <Share size="20" />
+            </span>
+            <span className={styles.label}>134 Share</span>
+          </button>
+          <button className={styles.button}>
+            <span className={styles.icon}>
+              <Bookmark size="20" />
+            </span>
+            <span className={styles.label}>16 Saved</span>
+          </button>
+        </div>
+
+        <p className={styles.description}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Quisquam, quod. Quisquam, quod.
+        </p>
+      </div>
+
+      <footer className={styles.comment}>
+        <Avatar src="https://i.pinimg.com/236x/3a/72/58/3a72584976987b1458c7330eb5638966.jpg" size="md" alt="avatar" />
+
+        <label className={styles.comment_input}>
+          <input className={styles.input} type="text" placeholder="Add a comment..." />
+
+          <div className={styles.options}>
+            <label className={styles.button}>
+              <Attachment size="20" />
+              <input type="file" />
+            </label>
+
+            <button className={styles.button}>
+              <Emotion size="20" />
+            </button>
+
+            <label className={styles.button}>
+              <ImageIcon size="20" />
+              <input type="file" />
+            </label>
+          </div>
+        </label>
+      </footer>
+    </section>
+  )
+}
