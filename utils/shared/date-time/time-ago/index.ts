14f149d60208c73b3e646288defe1c4257384246
itsmeleonardo <carlosgamercovi20@gmail.com>
2023-01-26 16:30:11 -0500

    add time ago

---
+++ utils/shared/date-time/time-ago/index.ts
@@ -0,0 +1,15 @@
+import { DateTime } from 'luxon'
+
+const units: Intl.RelativeTimeFormatUnit[] = ['year', 'month', 'week', 'day', 'hour', 'minute', 'second']
+
+export const timeAgo = (dateTime: DateTime | string | Date) => {
+  const date = DateTime.fromISO(dateTime.toString())
+
+  const diff = date.diffNow().shiftTo(...units)
+  const unit = units.find((unit) => diff.get(unit) !== 0) || 'second'
+
+  const relativeFormatter = new Intl.RelativeTimeFormat('es', {
+    numeric: 'auto',
+  })
+  return relativeFormatter.format(Math.trunc(diff.as(unit)), unit)
+}
