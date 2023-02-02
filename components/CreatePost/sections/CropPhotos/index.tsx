3d04c5dc2294a53b8ce8dc0d8ad09c11c555e8cd
itsmeleonardo <carlosgamercovi20@gmail.com>
2023-02-01 19:25:33 -0500

    fix default crop

--- components/CreatePost/sections/CropPhotos/index.tsx
+++ components/CreatePost/sections/CropPhotos/index.tsx
@@ -64,7 +64,7 @@ export default function CropPhotos() {
   // const [zoom, setZoom] = useState(0)
 
   useEffect(() => {
-    if (currentPhoto) {
+    if (defaultCrop) {
       setCrop(defaultCrop)
     }
 
