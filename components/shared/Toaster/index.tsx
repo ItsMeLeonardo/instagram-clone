4eadc4656dec02e40c1a6998a7028cace2996c4a
itsmeleonardo <carlosgamercovi20@gmail.com>
2023-02-04 11:54:10 -0500

    add animations to toast

--- components/shared/Toaster/index.tsx
+++ components/shared/Toaster/index.tsx
@@ -1,21 +1,35 @@
 'use client'
 import CloseLineIcon from 'remixicon-react/CloseLineIcon'
 import toast, { Toaster } from 'react-hot-toast'
+import { AnimationProps, motion } from 'framer-motion'
 
 import styles from './toaster.module.css'
+
+const toastAnimation: AnimationProps = {
+  initial: {
+    opacity: 0,
+    scale: 0.5,
+    originX: 0,
+    originY: '100%',
+  },
+  animate: {
+    opacity: 1,
+    scale: 1,
+  },
+}
 
 export default function ToastContainer() {
   return <Toaster position="bottom-left"></Toaster>
 }
 export const alertToast = (message: string) =>
   toast.custom((t) => (
-    <aside className={styles.container}>
+    <motion.aside {...toastAnimation} className={styles.container}>
       <picture className={styles.icon}>
         <img src="/assets/emoji/persevering-face.webp" alt="alert" />
       </picture>
       <div className={styles.message}>{message}</div>
-      <button className={styles.close} onClick={() => toast.dismiss(t.id)}>
+      <button className={styles.close} onClick={() => toast.remove(t.id)}>
         <CloseLineIcon />
       </button>
-    </aside>
+    </motion.aside>
   ))
