d2a08e507774a711b040af188c6c9a4a6809c33e
itsmeleonardo <carlosgamercovi20@gmail.com>
2023-02-05 18:26:59 -0500

    add tooltip animation

--- components/shared/Tooltip/index.tsx
+++ components/shared/Tooltip/index.tsx
@@ -1,4 +1,6 @@
 'use client'
+import { useSpring, motion } from 'framer-motion'
+
 import LazyTippy from './LazyTooltip'
 
 import type { TippyProps } from '@tippyjs/react'
@@ -12,13 +14,38 @@ type TooltipProps = {
 } & TippyProps
 
 export default function Tooltip({ children, content, ...props }: TooltipProps) {
+  const springConfig = { damping: 15, stiffness: 300 }
+  const initialScale = 0.5
+  const opacity = useSpring(0, springConfig)
+  const scale = useSpring(initialScale, springConfig)
+
+  function onMount() {
+    scale.set(1)
+    opacity.set(1)
+  }
+
+  function onHide({ unmount }: { unmount: () => void }) {
+    const cleanup = scale.on('change', (value) => {
+      if (value <= initialScale) {
+        cleanup()
+        unmount()
+      }
+    })
+
+    scale.set(initialScale)
+    opacity.set(0)
+  }
+
   return (
     <LazyTippy
       {...props}
+      animation={true}
+      onMount={onMount}
+      onHide={onHide}
       render={(attrs) => (
-        <div className={styles.container} tabIndex={-1} {...attrs}>
+        <motion.div style={{ scale, opacity }} className={styles.container} tabIndex={-1} {...attrs}>
           {content}
-        </div>
+        </motion.div>
       )}
     >
       {/* @ts-ignore */}
