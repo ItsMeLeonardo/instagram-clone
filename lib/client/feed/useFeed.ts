c462e2ae6d0e36146174ad0f9d07197340446021
itsmeleonardo <carlosgamercovi20@gmail.com>
2023-02-05 18:18:27 -0500

    fix swr revalidation

--- lib/client/feed/useFeed.ts
+++ lib/client/feed/useFeed.ts
@@ -6,7 +6,9 @@ import type { Post } from 'types/post'
 import { getUserFeed } from 'service/client/feed'
 
 export function useFeed(filter: FeedFilter) {
-  const { data, error, isLoading } = useSwr<Post[]>('userFeed', () => getUserFeed(filter))
+  const { data, error, isLoading } = useSwr<Post[]>('userFeed', () => getUserFeed(filter), {
+    revalidateOnFocus: false,
+  })
 
   return {
     posts: data,
