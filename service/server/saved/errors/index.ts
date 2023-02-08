2959e0ac1f5a05f095392c453b504ba8a3c5efd5
itsmeleonardo <carlosgamercovi20@gmail.com>
2023-02-08 08:49:08 -0500

    add saved error

--- service/server/saved/errors/index.ts
+++ service/server/saved/errors/index.ts
@@ -3,3 +3,9 @@ export class SavedListNotFoundError extends Error {
     super('Saved list not found')
   }
 }
+
+export class SavedListNotCreatedError extends Error {
+  constructor() {
+    super('Saved list not created')
+  }
+}
