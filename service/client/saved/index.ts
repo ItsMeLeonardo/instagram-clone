9001f07ff5a50b5b16e269be7d110bd5f3752f6e
itsmeleonardo <carlosgamercovi20@gmail.com>
2023-02-08 08:50:16 -0500

    add client service

--- service/client/saved/index.ts
+++ service/client/saved/index.ts
@@ -6,3 +6,17 @@ export async function getSavedPosts() {
 
   return data
 }
+
+export async function createSavedList(title: string) {
+  const { data } = await api.post<SavedList>('/saved', { title })
+
+  return data
+}
+
+export async function deleteSavedList(listId: number) {
+  await api.delete(`/saved/${listId}`)
+}
+
+export async function editSavedList(listId: number, title: string) {
+  await api.put(`/saved/${listId}`, { title })
+}
