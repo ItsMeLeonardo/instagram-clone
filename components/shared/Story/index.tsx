4f4640715471192423f4d1086be7f8abcaa34a79
itsmeleonardo <carlosgamercovi20@gmail.com>
2023-02-17 09:16:20 -0500

    fix remove comments

--- components/shared/Story/index.tsx
+++ components/shared/Story/index.tsx
@@ -48,34 +48,6 @@ export default function Story({ stories }: StoryProps) {
       ))}
       <Modal open={modalOpen} onClose={closeModal}>
         {selectedStory.current && <StorySlide story={selectedStory.current} onCompleted={closeModal} />}
-        {/*     <div className={styles.story_modal}>
-          <div className={styles.top_steps}>
-            <div className={styles.step}>
-              <span className={styles.progress} style={{ width: '10%' }}></span>
-            </div>
-            <div className={styles.step}>
-              <span className={styles.progress}></span>
-            </div>
-            <div className={styles.step}>
-              <span className={styles.progress}></span>
-            </div>
-          </div>
-          <header className={styles.header}>
-            <Avatar src={selectedStory.current?.avatar} size="md" alt="Story" />
-            <div className={styles.info}>
-              <span className={styles.username}>{selectedStory.current?.username}</span>
-              <span className={styles.time}>
-                {selectedStory.current?.createdAt && timeAgo(selectedStory.current?.createdAt)}
-              </span>
-            </div>
-          </header>
-          <picture className={styles.photo}>
-            <img
-              src="https://images.unsplash.com/photo-1675789652767-d3bc9f56abd7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8&auto=format&fit=crop&w=1400&q=60"
-              alt=""
-            />
-          </picture>
-        </div> */}
       </Modal>
       <CreateStory open={addStoryModal} onClose={closeAddStoryModal} />
     </aside>
