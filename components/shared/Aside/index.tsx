f5ce98ae8bfe677599f0d7f82a6d908b8262828c
itsmeleonardo <carlosgamercovi20@gmail.com>
2023-02-18 18:22:53 -0500

    fix lint errors

--- components/shared/Aside/index.tsx
+++ components/shared/Aside/index.tsx
@@ -2,13 +2,11 @@ import Send from 'remixicon-react/SendPlaneLineIcon'
 import Notification from 'remixicon-react/NotificationLineIcon'
 import Menu from 'remixicon-react/MenuLineIcon'
 
-import User from 'components/shared/User'
 import Section from './Section'
 
 import styles from './Aside.module.css'
 import Trending from './Trending'
-import { Suspense } from 'react'
-import TrendingLoader from '../Loader'
+
 import Suggestions from './Suggestions'
 
 export default function Aside() {
--- service/client/Suggestions/index.ts
+++ service/client/Suggestions/index.ts
@@ -2,7 +2,6 @@ import { Suggestion } from 'types/suggestions'
 import { api } from '../api'
 
 export async function getSuggestions() {
-  console.log('exe')
   const { data } = await api.get<Suggestion[]>('/suggestions')
   return data
 }
